2df382b5e3a7401d487df86744babd39c330814a
aniket450 <67451673+aniket450@users.noreply.github.com>
2020-07-07 12:44:20 +0530

    Update call_applyconfig.m

--- call_applyconfig.m
+++ call_applyconfig.m
@@ -1,6 +1,6 @@
 function [ret]=call_applyconfig()
 try
-    addpath('D:\Aniket\Sample_modelss')
+    addpath('D:\Aniket\Sample_models')
     apply_config_setting()
 catch
     disp('Erro in calling function')
